8ae6fcafda17ca5450c94a49064c05b1ab7ab9e1
NimmalaNithin <60269489+NimmalaNithin@users.noreply.github.com>
2024-10-25 16:21:55 -0400

    learnt to inject custom properties from application.properties

--- src/main/java/com/springboot/example/myapp/rest/FunRestController.java
+++ src/main/java/com/springboot/example/myapp/rest/FunRestController.java
@@ -1,10 +1,26 @@
 package com.springboot.example.myapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    // inject properties for coach.name and team.name
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    // expose '/teaminfo' endpoint that returns the injected coachName and teamName
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "Coach: " + coachName + ", Team Name: " + teamName;
+    }
+
+
     //expose '/' endpoint that simply returns 'hello world'
     @GetMapping("/")
     public String index() {
