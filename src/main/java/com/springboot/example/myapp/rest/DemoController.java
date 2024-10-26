5f6a0631062230cbb6fd3677b7a3a6f0b07198a7
NimmalaNithin <60269489+NimmalaNithin@users.noreply.github.com>
2024-10-26 01:15:41 -0400

    learnt field injection

--- src/main/java/com/springboot/example/myapp/rest/DemoController.java
+++ src/main/java/com/springboot/example/myapp/rest/DemoController.java
@@ -7,21 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
-    // define a private field for dependency
-    private Coach coach;
-    private Coach myCoach;
-
-    //define a setter for dependency injection
+    // define a private field for dependency injection
     @Autowired
-    public void setCoach(Coach coach) {
-        this.coach = coach;
-    }
-
-    //define a custom method for dependency injection
-//    @Autowired
-//    public void doSomeStuff(Coach coach) {
-//        myCoach = coach;
-//    }
+    private Coach coach;
 
     @GetMapping("/dailyworkout")
     public String getDailyWorkout(){
