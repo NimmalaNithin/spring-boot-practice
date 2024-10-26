288e580a41eb84e8ec2c9bb0c18c7cb77977e852
NimmalaNithin <60269489+NimmalaNithin@users.noreply.github.com>
2024-10-26 00:08:10 -0400

    learnt about setter injection

--- src/main/java/com/springboot/example/myapp/MyappApplication.java
+++ src/main/java/com/springboot/example/myapp/MyappApplication.java
@@ -3,10 +3,7 @@ package com.springboot.example.myapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(
-		scanBasePackages = {"com.springboot.example.myapp",
-							"com.springboot.example.util"}
-)
+@SpringBootApplication
 public class 	MyappApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/springboot/example/myapp/rest/DemoController.java
+++ src/main/java/com/springboot/example/myapp/rest/DemoController.java
@@ -9,12 +9,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
     // define a private field for dependency
     private Coach coach;
+    private Coach myCoach;
 
-    //define a constructor for dependency injection
+    //define a setter for dependency injection
     @Autowired
-    public DemoController(Coach coach) {
+    public void setCoach(Coach coach) {
         this.coach = coach;
     }
+
+    //define a custom method for dependency injection
+//    @Autowired
+//    public void doSomeStuff(Coach coach) {
+//        myCoach = coach;
+//    }
 
     @GetMapping("/dailyworkout")
     public String getDailyWorkout(){
