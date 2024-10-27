a3148335dd5c54bb7c01ca0db91866d57163a4aa
NimmalaNithin <60269489+NimmalaNithin@users.noreply.github.com>
2024-10-26 23:58:03 -0400

    learnt @Primary annotation

--- src/main/java/com/springboot/example/myapp/rest/DemoController.java
+++ src/main/java/com/springboot/example/myapp/rest/DemoController.java
@@ -2,7 +2,6 @@ package com.springboot.example.myapp.rest;
 
 import com.springboot.example.myapp.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +11,7 @@ public class DemoController {
     private Coach coach;
 
     @Autowired
-    public DemoController(@Qualifier("tennisCoach") Coach coach) {
+    public DemoController(Coach coach) {
         this.coach = coach;
     }
 
