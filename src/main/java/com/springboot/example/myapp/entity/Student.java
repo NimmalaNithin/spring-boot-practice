bd36c5c0ab944ebb4f319698bf6211a68deb6ad7
NimmalaNithin <60269489+NimmalaNithin@users.noreply.github.com>
2024-10-30 18:36:45 -0400

    learnt all CRUD operations to save Java objects into Database using DAO(Data Access Object)

--- src/main/java/com/springboot/example/myapp/entity/Student.java
+++ src/main/java/com/springboot/example/myapp/entity/Student.java
@@ -22,6 +22,9 @@ public class Student {
     private String email;
 
     //define constructors
+    public  Student() {
+    }
+
     public Student(String firstName, String lastName, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
