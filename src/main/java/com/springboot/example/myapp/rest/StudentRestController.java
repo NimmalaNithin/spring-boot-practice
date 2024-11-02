b5c426e1d3cbe90de4483e059ab9a13d5bcd575d
NimmalaNithin <60269489+NimmalaNithin@users.noreply.github.com>
2024-11-02 16:22:03 -0400

    learnt global exception handling

--- src/main/java/com/springboot/example/myapp/rest/StudentRestController.java
+++ src/main/java/com/springboot/example/myapp/rest/StudentRestController.java
@@ -42,32 +42,4 @@ public class StudentRestController {
         //just index into the list besed on index of the list...
         return students.get(studentId);
     }
-
-    //Add an exception handler using @ExceptionHandler
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc){
-
-        //create a StudentErrorResponse
-        StudentErrorResponse error = new StudentErrorResponse();
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(exc.getMessage());
-        error.setTimestamp(System.currentTimeMillis());
-
-        //return ResponseEntity
-        return new ResponseEntity<>(error,HttpStatus.NOT_FOUND);
-    }
-
-    //add another exception handler... to catch any exception (catch all)
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(Exception exc){
-        //create a StudentErrorResponse
-        StudentErrorResponse error = new StudentErrorResponse();
-        error.setStatus(HttpStatus.BAD_REQUEST.value());
-        //instead of exc.getMessage() you can give any custom message
-        error.setMessage(exc.getMessage());
-        error.setTimestamp(System.currentTimeMillis());
-
-        //return ResponseEntity
-        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
-    }
 }
