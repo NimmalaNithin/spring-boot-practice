ddc18e962988ec0437e9e7824d5628640ecc1172
NimmalaNithin <60269489+NimmalaNithin@users.noreply.github.com>
2024-11-04 16:22:00 -0500

    learnt authorization based on roles

--- src/main/java/com/springbootexample/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/springbootexample/cruddemo/dao/EmployeeRepository.java
@@ -2,9 +2,7 @@ package com.springbootexample.cruddemo.dao;
 
 import com.springbootexample.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     //that's it... no need to write any code
 }
---
+++ src/main/java/com/springbootexample/cruddemo/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.springbootexample.cruddemo.service;
+
+import com.springbootexample.cruddemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> findAll();
+
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
+
+}
